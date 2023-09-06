f9ace0d34f2f103a4e4c1ef1826d8f2acfb168fc
skeapskeap <4rjgtnfy5>
2023-09-06 09:46:50 +0700

    level_1 test_genderalize

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,17 @@
+import pytest
+
 from functions.level_1.one_gender import genderalize
 
 
-def test_genderalize():
-    pass
+@pytest.mark.parametrize("gender, verb_male, verb_fem, expected",
+                         [
+                             ("male", "жил", "жила", "жил"),
+                             ("female", "жил", "жила", "жила"),
+                             ("other", "жил", "жила", "жила"),
+                             (None, "жил", "жила", "жила"),
+                         ])
+def test_genderalize(gender, verb_male, verb_fem, expected):
+    genderalized_verb = genderalize(verb_male=verb_male,
+                                    verb_female=verb_fem,
+                                    gender=gender)
+    assert genderalized_verb == expected
