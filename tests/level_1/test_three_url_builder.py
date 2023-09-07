077d70cd73f8f86341ef4488e086cb907995158e
skeapskeap <4rjgtnfy5>
2023-09-07 10:18:56 +0700

    level_1 test_build_url

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,5 +1,16 @@
+import pytest
+
 from functions.level_1.three_url_builder import build_url
 
 
-def test_build_url():
-    pass
+@pytest.mark.parametrize(
+    "host, path, query_params, expected",
+    [
+        ("h", "p", {"q1": "v1", "q2": "v2"}, "h/p?q1=v1&q2=v2"),
+        ("h", "p", {"q1": "v1"}, "h/p?q1=v1"),
+        ("h", "p", {}, "h/p"),
+        ("h", "p", None, "h/p"),
+    ])
+def test_build_url(host, path, query_params, expected):
+    url = build_url(host, path, query_params)
+    assert url == expected
