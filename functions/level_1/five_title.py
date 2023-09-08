a5e64054e8a625bfeb773ab97e368340d379d463
skeapskeap <4rjgtnfy5>
2023-09-08 13:32:13 +0700

    level_1 refactor five_title.py

--- functions/level_1/five_title.py
+++ functions/level_1/five_title.py
@@ -1,16 +1,21 @@
 import re
 
+COPY_PREFIX = 'Copy of '
+
 
 def change_copy_item(title: str, max_main_item_title_length: int = 100) -> str:
-    additional_copy_text: str = 'Copy of'
-    title_with_additional_copy_text: str = f'{additional_copy_text} {title}'
+    title_with_additional_copy_text: str = f'{COPY_PREFIX}{title}'
 
     if len(title_with_additional_copy_text) >= max_main_item_title_length:
         return title
-    if not title.startswith(additional_copy_text):
+    if not title.startswith(COPY_PREFIX):
         return title_with_additional_copy_text
 
     last_element = title.split()[-1]
     element_in_brackets = last_element.split('(')[-1].split(')')[0]
-    has_copy_number = all([re.search(r'\(\d+\)', last_element), element_in_brackets.isdigit()])
-    return title.replace(title.split()[-1], f'({int(element_in_brackets) + 1})') if has_copy_number else f'{title} (2)'
+    has_copy_number = all(
+        [re.search(r'\(\d+\)', last_element), element_in_brackets.isdigit()]
+    )
+    return title.replace(
+        title.split()[-1], f'({int(element_in_brackets) + 1})'
+    ) if has_copy_number else f'{title} (2)'
