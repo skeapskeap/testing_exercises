ed1c3b92585d7ccd1eed8401efa843b83e3ba6b7
skeapskeap <4rjgtnfy5>
2023-09-11 12:18:33 +0700

    level_1 refactor #1 test__genderalize

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -9,7 +9,11 @@ def test_compose_datetime_from(date_test_case):
     assert composed_dt == date_test_case.dt_expected
 
 
-@pytest.mark.parametrize("time_str", ["no_delimiter", "not:integers", "25:00", "00:61"])
+# @pytest.mark.parametrize("time_str", ["no_delimiter", "not:integers", "25:00", "00:61"])
+@pytest.mark.parametrize('time_str', [
+    pytest.param('00:61', id='large minutes number'),
+    pytest.param('25:00', id='large hours number'),
+])
 def test_compose_datetime_from_raises(time_str):
     with pytest.raises(ValueError):
         compose_datetime_from(relative_day="", time_str=time_str)
