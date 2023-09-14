34dd356ddc0a5046fa25291e6f993a690fa813b9
skeapskeap <4rjgtnfy5>
2023-09-14 23:28:31 +0700

    level_1 change_copy_item() рефакторинг тестов

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,22 +1,44 @@
 import pytest
 
-from functions.level_1.five_title import change_copy_item, COPY_PREFIX
+from functions.level_1.five_title import change_copy_item
+
+COPY_PREFIX = "Copy of"
+CONJUNCTOR = " "
 
 
-@pytest.mark.parametrize(
-    "title, max_len, expected",
-    [
-        ("test_title", 10, "test_title"),
-        ("test_title", 10 + len(COPY_PREFIX), "test_title"),
-        ("test_title", 10 + len(COPY_PREFIX) + 1, f"{COPY_PREFIX}test_title"),
-        (f"{COPY_PREFIX}test_title", 10 + len(COPY_PREFIX) + 1, f"{COPY_PREFIX}test_title"),
-        (f"{COPY_PREFIX}test_title", 10 + 2 * len(COPY_PREFIX) + 1, f"{COPY_PREFIX}test_title (2)"),
-        (f"{COPY_PREFIX}test_title (3)", 14 + 2 * len(COPY_PREFIX), f"{COPY_PREFIX}test_title (3)"),
-        (f"{COPY_PREFIX}test_title (3)", 14 + 2 * len(COPY_PREFIX) + 1, f"{COPY_PREFIX}test_title (4)"),
-        (f"{COPY_PREFIX}test_title (0)", 14 + 2 * len(COPY_PREFIX) + 1, f"{COPY_PREFIX}test_title (1)"),
-        (f"{COPY_PREFIX}test_title (-1)", 15 + 2 * len(COPY_PREFIX) + 1, f"{COPY_PREFIX}test_title (-1) (2)"),
-    ]
-)
-def test_change_copy_item(title, max_len, expected):
-    res = change_copy_item(title, max_len)
-    assert res == expected
+@pytest.mark.parametrize("test_title", [
+    pytest.param("my_title", id="any custom title"),
+    pytest.param(f"{COPY_PREFIX} my title", id="title starts with copy prefix"),
+])
+def test__change_copy_item__too_small_max_len(test_title: str):
+    max_len_lte_resulting_title = len(test_title) + len(COPY_PREFIX) + len(CONJUNCTOR)
+    title = change_copy_item(test_title, max_len_lte_resulting_title)
+    assert title == test_title
+
+
+def test__change_copy_item__add_prefix():
+    test_title = "my_title"
+    max_len_gt_resulting_title = len(test_title) + len(COPY_PREFIX) + len(CONJUNCTOR) + 1
+    title = change_copy_item(test_title, max_len_gt_resulting_title)
+    assert title == f"{COPY_PREFIX} {test_title}"
+
+
+@pytest.mark.parametrize("test_title, expected", [
+    (f"{COPY_PREFIX}test_title", f"{COPY_PREFIX}test_title (2)"),
+    (f"{COPY_PREFIX}test_title (-1)", f"{COPY_PREFIX}test_title (-1) (2)"),
+])
+def test__change_copy_item__add_postfix(test_title: str, expected: str):
+    max_len_gt_resulting_title = len(test_title) + len(COPY_PREFIX) + len(CONJUNCTOR) + 1
+    title = change_copy_item(test_title, max_len_gt_resulting_title)
+    assert title == expected
+
+
+@pytest.mark.parametrize("test_title, expected", [
+    (f"{COPY_PREFIX}test_title (0)", f"{COPY_PREFIX}test_title (1)"),
+    (f"{COPY_PREFIX}test_title (1)", f"{COPY_PREFIX}test_title (2)"),
+    (f"{COPY_PREFIX}test_title (2)", f"{COPY_PREFIX}test_title (3)"),
+])
+def test__change_copy_item__increment_postfix(test_title: str, expected: str):
+    max_len_gt_resulting_title = len(test_title) + len(COPY_PREFIX) + len(CONJUNCTOR) + 1
+    title = change_copy_item(test_title, max_len_gt_resulting_title)
+    assert title == expected
